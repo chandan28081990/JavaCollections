2d1adc526303dd1ec9b042e0ef9887d47b30dfe2
Chandan <chandan325401@gmail.com>
2025-04-26 13:41:13 +0530

    Merge Commit Resolved.

--- JavaCollectionExample/src/com/java/project/collections/DummyCode.java
+++ JavaCollectionExample/src/com/java/project/collections/DummyCode.java
@@ -2,6 +2,7 @@ package com.java.project.collections;
 
 public class DummyCode {
     public static void main(String[] args){
+        System.out.println("Hi,all");
         System.out.println("Hi, How are you?");
     }
 }
