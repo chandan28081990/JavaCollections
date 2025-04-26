e6b7e6d7a76cf2f65fba4a958f837ca6c230d360
chandan28081990 <chandan28081990@gmail.com>
2025-04-26 13:27:23 +0530

    Update DummyCode.java

--- JavaCollectionExample/src/com/java/project/collections/DummyCode.java
+++ JavaCollectionExample/src/com/java/project/collections/DummyCode.java
@@ -2,6 +2,6 @@ package com.java.project.collections;
 
 public class DummyCode {
     public static void main(String[] args){
-        System.out.println("Hi");
+        System.out.println("Hi, How are you?");
     }
 }
